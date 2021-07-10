73b33c7f5582b659944b216c57279a49007fe1d6
TondoDev <tondodeveloper@gmail.com>
2021-07-10 21:58:28 +0200

    exchange service established

---
+++ currency-exange-service/src/main/java/org/tondo/bootms/exchange/CurrencyExangeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tondo.bootms.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExangeServiceApplication.class, args);
+	}
+
+}
---
+++ currency-exange-service/src/main/java/org/tondo/bootms/exchange/controller/CurrencyExchange.java
@@ -0,0 +1,64 @@
+package org.tondo.bootms.exchange.controller;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultipe;
+	// for marking instance of service
+	private String environment;
+	
+	
+	public CurrencyExchange() {
+		
+	}
+	
+	
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultipe) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultipe = conversionMultipe;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getConversionMultipe() {
+		return conversionMultipe;
+	}
+	public void setConversionMultipe(BigDecimal conversionMultipe) {
+		this.conversionMultipe = conversionMultipe;
+	}
+	
+	
+	public String getEnvironment() {
+		return environment;
+	}
+	
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+	
+}
