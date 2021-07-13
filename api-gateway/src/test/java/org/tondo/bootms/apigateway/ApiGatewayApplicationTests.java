f0a5e9e45531524b2e34f71763dfa8ea6f974f27
TondoDev <tondodeveloper@gmail.com>
2021-07-13 23:03:34 +0200

    initial API gateway

---
+++ api-gateway/src/test/java/org/tondo/bootms/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tondo.bootms.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
